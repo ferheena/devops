9ca1eae9987d374f0f280765e8d1347647a2ff16
root <root@sonaqube-1.asia-south1-c.c.softethervpn.internal>
2019-10-22 10:24:17 +0000

    folder

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,18 @@
+import sys
+sys.path.append('/opt/hello_world/app')
 import pytest
 from app import *
 
 @pytest.fixture
 def client():
     client = app.test_client()
+    #client = 'Hello world!'
     return client
 
 def test_root(client):
     """Test the default route."""
 
     res = client.get('/')
+    print res
+    print res.data
     assert b'Hello world!' in res.data
